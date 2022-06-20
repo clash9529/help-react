67ccff756d50dbc462fad7153fead815ba777974
Jiahui <clash9529@gmail.com>
2022-06-20 23:59:11 +0800

    add elements

--- src/new_deceased.js
+++ src/new_deceased.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
 import "./App.css"
 
-
 function NewDetails(){
+    const dataUrl = "https://jsonplaceholder.typicode.com/users";
+
+    // const dataUrl = "http://localhost:5000/deceaseds";
+    
+    const [deceased, setDeceased] = useState([]);
+
+    useEffect(() => {
+        getDeceased();
+    }, []);
+
+    async function getDeceased() {
+        const response = await fetch(dataUrl);
+        const data = await response.json();
+        
+        setDeceased(data);
+        console.log(data);
+
+    }
     return(
         <div className='App-header'>
-            <h1>New Deceased Information</h1>
+            <h1>New deceased details</h1>
+            {deceased.map((value) => (
+                <div>
+                    <h5>Name: {value.name}</h5>
+                    <hr/>
+                </div>
+            ))}
         </div>
     );
 }
--- src/view_details.js
+++ src/view_details.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
 import "./App.css"
-
+import Moment from 'moment';
 
 function ViewDetails(){
+    // const dataUrl = "https://jsonplaceholder.typicode.com/users";
+
+    const dataUrl = "http://localhost:5000//deceased_posts/1";
+    Moment.locale('en');
+    
+    const [deceased, setDeceased] = useState([]);
+
+    useEffect(() => {
+        getDeceased();
+    }, []);
+
+    async function getDeceased() {
+        const response = await fetch(dataUrl, {
+            mode:'cors'
+        });
+        const data = await response.json();
+        
+        setDeceased(data);
+        console.log(data);
+
+    }
     return(
         <div className='App-header'>
-            <h1>View deceased details</h1>
+            <h1>{deceased.name}, {deceased.age}</h1>
+            <div>
+                <h5>Date of Birth: {Moment(deceased.created_at).format('Do MMMM YYYY')}</h5>
+                <h5>Description: {deceased.description}</h5>
+                <hr/>
+            </div>
         </div>
     );
 }
